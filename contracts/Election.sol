a3032dae3069efedf17e054e0a1fbdce56f62988
malverick <sumitlnmiit96@gmail.com>
2018-11-24 16:30:22 +0530

    List candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,27 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-	// Store candidate
-	// Read candidate
-	string public candidate;
-	// Constructor
+	// Model a candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	// Store candidates
+	
+	// Fetch candidate
+	mapping(uint => Candidate) public candidates;
+	// Store candidates count
+	uint public candidatesCount;
+
 	function Election () public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate (string _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
